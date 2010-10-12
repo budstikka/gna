722b12b9c0465abb6691e26a0b37ff8b4f89d458
Peter Haza <peter.haza@gmail.com>
2010-10-12 09:58:20 +0200

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gna::Application.config.secret_token = '6a14563f725972b37e731af5fac5d8110629c37b9520865e9bf82a711dbdb694aff6ce1cd7ba89ab5f2f8533facb7d659e7a3916b61bb255daccadf103cd8297'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gna::Application.config.session_store :cookie_store, :key => '_gna_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Gna::Application.config.session_store :active_record_store
